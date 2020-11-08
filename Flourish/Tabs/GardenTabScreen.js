85738d3f2f9cae7dc0ddf02e775617f337b8d757
rainyuxuan <yu2001xuan@outlook.com>
2020-11-08 02:34:49 -0500

    garden modal

--- Flourish/Tabs/GardenTabScreen.js
+++ Flourish/Tabs/GardenTabScreen.js
@@ -1,21 +1,106 @@
-import * as React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity, Modal, Image, SectionList } from 'react-native';
 // import { TouchableOpacity } from 'react-native-gesture-handler';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import PlantCard from '../Components/Garden/PlantCard'
+// import PlantCard from '../Components/Garden/PlantCard'
+
+
+class PlantCard extends React.Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 0.8,
+                    height: 150,
+                    // width: '100%',
+                    flexDirection: 'row',
+                    alignItems: 'flex-start',
+                    marginBottom: 8,
+                    marginHorizontal: 10,
+
+                    backgroundColor: '#fff',
+                    borderRadius: 10,
+
+                    // shadowColor: "#000",
+                    // shadowOffset: {
+                    //     width: 0,
+                    //     height: 4,
+                    // },
+                    // shadowOpacity: 0.32,
+                    // shadowRadius: 5.46,
+
+                    // elevation: 9,
+                }}>
+                <View style={{
+                    flex: 1,
+                    borderTopLeftRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    padding: 8,
+                }}>
+                    <Image
+                        style={{
+                            flex: 1,
+                            borderRadius: 10,
+                        }}
+                        source={{ uri: this.props.data.img }}
+                    />
+                </View>
+                <View style={{
+                    flexDirection: 'column',
+                    padding: 8,
+                    flex: 1,
+                }}>
+                    <Text style={{
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                    }}>
+                        {this.props.data.name}
+                    </Text>
+                    <Text style={{
+                        fontSize: 12,
+                    }}>
+                        {this.props.data.binomial}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+}
 
 export default function GardenTabScreen() {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [currPlant, setCurrPlant] = useState({});
+
     const renderItem = ({ item }) => {
         // if (item.id < DATA.length - 1) {
-            return (
+        // console.log(item)
+        return (
+            <TouchableOpacity
+                onPress={() => {
+                    console.log(1);
+                    setCurrPlant({
+                        "id": item.id,
+                        "img": item.img,
+                        "name": item.name,
+                        "binomial": item.binomial,
+                        "intro": item.intro,
+                        "maintain": item.maintain,
+                        // "reminder": item.reminder,
+                    })
+                    setModalVisible(true);
+                }}>
                 <PlantCard
                     data={{
+                        "id": item.id,
                         "img": item.img,
                         "name": item.name,
                         "binomial": item.binomial,
                     }}
+
+
                 />
-            );
+            </TouchableOpacity>
+        );
         // } else {
         //     return (
         //         <View style={{
@@ -38,6 +123,65 @@ export default function GardenTabScreen() {
                 renderItem={renderItem}
                 keyExtractor={item => item.id}
             />
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+            >
+                <View style={modalStyles.centeredView}>
+                    <ScrollView style={modalStyles.modalView}>
+                        <TouchableOpacity
+                            style={{ width: 50, height: 50, position: 'absolute', top: -7, left: -14, zIndex: 10 }}
+                            onPress={() => {
+                                setModalVisible(!modalVisible);
+                            }}
+                        >
+                            <MaterialCommunityIcons name="chevron-left" color="#CACACA" size={40} />
+                        </TouchableOpacity>
+                        <View style={modalStyles.rowContainer}>
+                            <View style={{ ...modalStyles.plantImage, marginBottom: 16, }}>
+                                <Image
+                                    style={modalStyles.plantImage}
+                                    source={{ uri: currPlant.img }}
+                                />
+                            </View>
+                            <Text style={modalStyles.plantName}>{currPlant.name}</Text>
+                            <Text style={modalStyles.plantBinomial}>{currPlant.binomial}</Text>
+                        </View>
+                        <View style={modalStyles.rowContainer}>
+                            <Text style={modalStyles.plantIntro}>
+                                {currPlant.intro}
+                            </Text>
+                        </View>
+                        <View style={modalStyles.rowContainer}>
+                            <View style={{ ...modalStyles.mrContainer, backgroundColor: '#ffffff' }}>
+                                <Text style={{ ...modalStyles.mrContainerHeader, color: '#505050' }}>MAINTAIN</Text>
+                                {currPlant.maintain.map(i => (
+                                    <View style={modalStyles.mrItem}>
+                                        <Text style={modalStyles.mrItemText}>{i.title}</Text>
+                                        <Text style={modalStyles.mrItemText}>{i.spec}</Text>
+                                    </View>
+                                ))}
+                                {
+                                    console.log(currPlant.maintain)
+                                }
+                            </View>
+                        </View>
+                        <View style={modalStyles.rowContainer}>
+                            <View style={{ ...modalStyles.mrContainer, backgroundColor: '#EF5E85' }}>
+                                <Text style={{ ...modalStyles.mrContainerHeader, color: '#ffffff' }}>REMINDER</Text>
+                                {/* {currPlant.reminder.map(i => (
+                                    <View style={{...modalStyles.mrItem, backgroundColor:'#ffffff'}}>
+                                        <Text style={modalStyles.mrItemText}>{i.task}</Text>
+                                        <Text style={modalStyles.mrItemText}>{i.time}</Text>
+                                    </View>
+                                ))} */}
+                            </View>
+                        </View>
+
+                    </ScrollView>
+                </View>
+            </Modal>
         </SafeAreaView>
     );
 }
@@ -77,6 +221,108 @@ const styles = StyleSheet.create({
         marginHorizontal: 16,
     },
 });
+const modalStyles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#00000020',
+        // marginTop: 22
+    },
+    modalView: {
+        flex: 1,
+        width: '90%',
+        marginTop: 16,
+        marginBottom: 32,
+        backgroundColor: "#f0f0f0",
+        borderRadius: 10,
+        paddingHorizontal: 32,
+        paddingVertical: 32,
+        // alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    openButton: {
+        backgroundColor: "#F194FF",
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    rowContainer: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+    },
+
+    plantImage: {
+        width: 150,
+        height: 200,
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+    plantName: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#505050'
+    },
+    plantBinomial: {
+        fontSize: 13,
+        color: '#505050',
+        fontStyle: 'italic',
+    },
+    plantIntro: {
+        marginVertical: 16,
+        marginHorizontal: 0,
+        textAlign: 'left',
+        color: '#505050',
+    },
+    mrContainer: {
+        width: '100%',
+        marginVertical: 8,
+        padding: 16,
+        // backgroundColor: '#ffffff',
+        borderRadius: 10,
+    },
+    mrContainerHeader: {
+        textAlign: 'left',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+        marginBottom: 5,
+    },
+    mrItem: {
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        marginVertical: 3,
+        marginHorizontal: -5,
+        padding: 5,
+        // backgroundColor: '#f0f0f0',
+        borderRadius: 5,
+    },
+    mrItemText: {
+        color: '#505050',
+        fontSize: 13,
+    }
+})
 
 
 const DATA = [
@@ -85,30 +331,135 @@ const DATA = [
         "img": "https://i1.kknews.cc/SIG=52b418/26r5000002r82qro63ro.jpg",
         "name": "My Red Plant",
         "binomial": "Echeveria Apus",
+        "intro": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius ipsum eveniet laboriosam dolore perspiciatis, numquam iusto provident nobis, commodi voluptatem repellat nemo, quae voluptate facilis fuga labore in? Debitis, minus!",
+        "maintain": [
+            {
+                "title": "SUNSHINE",
+                "spec": "Full daylight",
+            },
+            {
+                "title": "WATERING",
+                "spec": "1 per week",
+            },
+        ],
+        // "reminder": [
+        //     {
+        //         "task": "WATERING",
+        //         "time": "Wed | 1 week",
+        //     },
+        //     {
+        //         "task": "FERTILIZING",
+        //         "time": "Thu | 2 week",
+        //     },
+        // ],
     },
     {
         "id": "1",
         "img": "https://www.zhifure.com/upload/images/2018/3/27164120681.jpg",
         "name": "The Succulents",
         "binomial": "Echeveria Apus",
+        "intro": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius ipsum eveniet laboriosam dolore perspiciatis, numquam iusto provident nobis, commodi voluptatem repellat nemo, quae voluptate facilis fuga labore in? Debitis, minus!",
+        "maintain": [
+            {
+                "title": "SUNSHINE",
+                "spec": "Full daylight",
+            },
+            {
+                "title": "WATERING",
+                "spec": "1 per week",
+            },
+        ],
+        // "reminder": [
+        //     {
+        //         "task": "WATERING",
+        //         "time": "Wed | 1 week",
+        //     },
+        //     {
+        //         "task": "FERTILIZING",
+        //         "time": "Thu | 2 week",
+        //     },
+        // ],
     },
     {
         "id": "2",
         "img": "https://duorou.zw3e.com/uploads/allimg/180828/3-1PRQ12410227.jpg",
         "name": "Greens",
         "binomial": "Echeveria Apus",
+        "intro": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius ipsum eveniet laboriosam dolore perspiciatis, numquam iusto provident nobis, commodi voluptatem repellat nemo, quae voluptate facilis fuga labore in? Debitis, minus!",
+        "maintain": [
+            {
+                "title": "SUNSHINE",
+                "spec": "Full daylight",
+            },
+            {
+                "title": "WATERING",
+                "spec": "1 per week",
+            },
+        ],
+        // "reminder": [
+        //     {
+        //         "task": "WATERING",
+        //         "time": "Wed | 1 week",
+        //     },
+        //     {
+        //         "task": "FERTILIZING",
+        //         "time": "Thu | 2 week",
+        //     },
+        // ],
     },
     {
         "id": "3",
         "img": "https://i.epochtimes.com/assets/uploads/2020/05/shutterstock_1714301299-600x400.jpg",
         "name": "Yeah",
         "binomial": "Echeveria Apus",
+        "intro": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius ipsum eveniet laboriosam dolore perspiciatis, numquam iusto provident nobis, commodi voluptatem repellat nemo, quae voluptate facilis fuga labore in? Debitis, minus!",
+        "maintain": [
+            {
+                "title": "SUNSHINE",
+                "spec": "Full daylight",
+            },
+            {
+                "title": "WATERING",
+                "spec": "1 per week",
+            },
+        ],
+        // "reminder": [
+        //     {
+        //         "task": "WATERING",
+        //         "time": "Wed | 1 week",
+        //     },
+        //     {
+        //         "task": "FERTILIZING",
+        //         "time": "Thu | 2 week",
+        //     },
+        // ],
     },
     {
         "id": "4",
         "img": "https://i.zw3e.com/zw_news_map/550/2017121/1512527973023359675.jpg",
         "name": "Leaves",
         "binomial": "Echeveria Apus",
+        "intro": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius ipsum eveniet laboriosam dolore perspiciatis, numquam iusto provident nobis, commodi voluptatem repellat nemo, quae voluptate facilis fuga labore in? Debitis, minus!",
+        "maintain": [
+            {
+                "title": "SUNSHINE",
+                "spec": "Full daylight",
+            },
+            {
+                "title": "WATERING",
+                "spec": "1 per week",
+            },
+        ],
+        // "reminder": [
+        //     {
+        //         "task": "WATERING",
+        //         "time": "Wed | 1 week",
+        //     },
+        //     {
+        //         "task": "FERTILIZING",
+        //         "time": "Thu | 2 week",
+        //     },
+        // ],
     },
-    // { "id": "5" }
 ]
+
--- Flourish/Tabs/PlantScreen.js
+++ Flourish/Tabs/PlantScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { View, StyleSheet, Text } from 'react-native';
 
-export default function PlantScreen() {
-    render() {
+export default class PlantScreen extends React.Component {
+    render(){
         return (
             <View>
                 <Text>Hello world</Text>
